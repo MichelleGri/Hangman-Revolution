c0e55c8554593c8cc3192b764f5fab0be2434048
MichelleGri <michelle0g@hotmail.com>
2022-10-13 17:46:00 +0000

    Create variable for player to guess a letter

--- run.py
+++ run.py
@@ -2,5 +2,16 @@ import random
 
 movies_list = ["Harry Potter", "Lord of the Rings", "The Godfather"]
 
+"""
+variable to choose a random movie from movies list
+"""
 secret_movie = random.choice(movies_list)
 print(secret_movie)
+
+"""
+ask player to guess a letter
+"""
+guessed_letter = input("Please guess a letter!\n").lower()
+print(guessed_letter)
+
+s
