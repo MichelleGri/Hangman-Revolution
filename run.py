245f9f95792b2948898d73c44256a9b8175fbf47
MichelleGri <michelle0g@hotmail.com>
2022-10-13 17:35:28 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+movies_list = ["Harry Potter", "Lord of the Rings", "The Godfather"]
+
+secret_movie = random.choice(movies_list)
+print(secret_movie)
