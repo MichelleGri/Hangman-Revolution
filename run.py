ca46a8a8e25c88439ad34f6c503b270c299518cf
MichelleGri <michelle0g@hotmail.com>
2022-10-14 12:07:14 +0000

    Create if statement for game over functionality

--- run.py
+++ run.py
@@ -8,6 +8,8 @@ variable to choose a random movie from movies list
 movie = random.choice(movies_list)
 print(movie)
 
+player_lives = 6
+
 """
 display "_" for movie
 """
@@ -21,20 +23,31 @@ for letter in range(movie_length):
         display_movie += "_"
 print(display_movie)
 
-
 """
 ask player to guess a letter
-"""
-guessed_letter = input("Please guess a letter\n").lower()
-print(guessed_letter)
 
-"""
 check if guessed letter is in the movie
 """
+game_over = False
 
-for p in range(movie_length):
-    letter = movie[p]
-    if letter == guessed_letter:
-        display_movie[p] = letter
+while not game_over:
+    guessed_letter = input("Please guess a letter\n").lower()
+    print(guessed_letter)
 
-print(display_movie)
+    for p in range(movie_length):
+        letter = movie[p]
+        if letter == guessed_letter:
+            display_movie[p] = letter
+            print("You lost the game!")
+
+    print(display_movie)
+
+    if guessed_letter not in movie:
+        player_lives -= 1
+        if player_lives == 0:
+            game_over = True
+
+    if "_" not in display_movie:
+        game_over = True
+        print("Congratulations! You guessed the correct movie!")
+
