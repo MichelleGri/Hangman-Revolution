6c5340e1580443456e3262568281d1235878a0d9
MichelleGri <michelle0g@hotmail.com>
2022-10-14 11:49:06 +0000

    Implement game over functionality

--- run.py
+++ run.py
@@ -5,8 +5,22 @@ movies_list = ["Harry Potter", "Lord of the Rings", "The Godfather"]
 """
 variable to choose a random movie from movies list
 """
-secret_movie = random.choice(movies_list)
-print(secret_movie)
+movie = random.choice(movies_list)
+print(movie)
+
+"""
+display "_" for movie
+"""
+
+display_movie = []
+movie_length = len(movie)
+for letter in range(movie_length):
+    if letter == "":
+        continue
+    else:
+        display_movie += "_"
+print(display_movie)
+
 
 """
 ask player to guess a letter
@@ -15,10 +29,12 @@ guessed_letter = input("Please guess a letter\n").lower()
 print(guessed_letter)
 
 """
-check if guessed letter is in the secret movie
+check if guessed letter is in the movie
 """
-for letter in secret_movie:
+
+for p in range(movie_length):
+    letter = movie[p]
     if letter == guessed_letter:
-        print("You have guessed the correct letter!")
-    else:
-        print(f"The letter {guessed_letter} is not in the secret movie!")
+        display_movie[p] = letter
+
+print(display_movie)
