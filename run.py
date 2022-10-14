5198e1d70cbd14897527d34bb5f2fd7ec641808e
MichelleGri <michelle0g@hotmail.com>
2022-10-14 14:11:40 +0000

    Create functionality for invalid guess

--- run.py
+++ run.py
@@ -3,20 +3,20 @@ from hangman_movies import movies_list
 from hangman_stages import stages
 
 """
-choose a random movie from movies list
+choose a random movie from list
 
 set player lives to 6
 
 number of letters in movie name
 
 set initial game over state to false
-
 """
 
 movie = random.choice(movies_list).upper()
 player_lives = 6
 movie_length = len(movie)
 game_over = False
+
 
 """
 display "_" for movie name
@@ -26,7 +26,7 @@ display_movie = []
 
 for letter in range(movie_length):
     if letter == " ":
-        continue
+        display_movie += " "
     else:
         display_movie += "_"
 
@@ -36,32 +36,36 @@ ask player to guess a letter
 check if guessed letter is in the movie
 """
 
-
 while not game_over:
-    guessed_letter = input("Please guess a letter\n").upper()
-    print(guessed_letter)
+    guess = input("Please guess a letter\n").upper()
 
-    if guessed_letter in display_movie:
-        print(f"You have aleady guessed the letter {guessed_letter}")
+    if len(guess) == 1 and guess.isalpha():
 
-    for p in range(movie_length):
-        letter = movie[p]
-        if letter == guessed_letter:
-            display_movie[p] = letter
-            print("You guessed a correct letter!")
+        if guess in display_movie:
+            print(f"You have aleady guessed the letter {guess}")
+
+        for p in range(movie_length):
+            letter = movie[p]
+            if letter == guess:
+                display_movie[p] = letter
+                print("You guessed a correct letter!")
             
-    print(display_movie)
+        print(display_movie)
 
-    if guessed_letter not in movie:
-        player_lives -= 1
-        print(f"The letter {guessed_letter} is not in this movie, you lose a life.")
-        if player_lives == 0:
+        if guess not in movie:
+            player_lives -= 1
+            print(f"The letter {guess} is not in this movie, you lose a life!")
+            if player_lives == 0:
+                game_over = True
+                print("You lost the game!")
+
+        if "_" not in display_movie:
             game_over = True
-            print("You lost the game!")
+            print("Congratulations! You guessed the correct movie!")
 
-    if "_" not in display_movie:
-        game_over = True
-        print("Congratulations! You guessed the correct movie!")
+    else:
+        ("Invalid guess, please guess letters from A-Z only!")
 
-    print(stages[player_lives])
+        print(stages[player_lives])
+    
 
