622d97369f0944620e8438b3fd4e43ac110c1034
MichelleGri <michelle0g@hotmail.com>
2022-10-14 12:25:52 +0000

    Create and import movies list and hangman stages files

---
+++ hangman_movies.py
@@ -0,0 +1,6 @@
+movies_list = [
+    "Harry Potter",
+    "The Lord of the Rings",
+    "Back to the Future",
+    "The Godfather"
+]
--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import random
-
-movies_list = ["Harry Potter", "Lord of the Rings", "The Godfather"]
+from hangman_movies import movies_list
 
 """
 variable to choose a random movie from movies list
@@ -38,16 +37,19 @@ while not game_over:
         letter = movie[p]
         if letter == guessed_letter:
             display_movie[p] = letter
-            print("You lost the game!")
-
+            
     print(display_movie)
 
     if guessed_letter not in movie:
         player_lives -= 1
+        print(f"The letter {guessed_letter} is not in this movie, you lose a life.")
         if player_lives == 0:
             game_over = True
+            print("You lost the game!")
 
     if "_" not in display_movie:
         game_over = True
         print("Congratulations! You guessed the correct movie!")
+
+    print(hangman_stages[player_lives])
 
